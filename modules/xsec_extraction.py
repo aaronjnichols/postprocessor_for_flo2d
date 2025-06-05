ab92c94dcfddf9dfc1ad50ce832c95976cc18d75
Aaron Nichols <136554330+aaronjnichols@users.noreply.github.com>
2025-06-04 18:53:08 -0700

    Refactor channel extraction into modular files

---
+++ modules/xsec_extraction.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+from .utilities import time_function
+
+@time_function
+def extract_xsec_dat(path):
+    """Extract cross section geometry from XSEC.DAT"""
+    file_path = os.path.join(path, 'XSEC.DAT')
+    data = []
+    with open(file_path, 'r') as file:
+        cross_section = None
+        for line in file:
+            if line.startswith('X'):
+                cross_section = int(line.split()[1])
+            elif line.strip():
+                station, elevation = map(float, line.split())
+                data.append((cross_section, station, elevation))
+    return pd.DataFrame(data, columns=['Cross Section Number', 'Station', 'Elevation'])
